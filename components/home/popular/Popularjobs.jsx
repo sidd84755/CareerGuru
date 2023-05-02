ad14c9e115f5f16828e98484e39a21c7260f1728
Siddharth Chaturvedi <sidd84755@gmail.com>
2023-05-02 22:41:28 +0530

    Worked on bugs

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -13,7 +13,12 @@ const Popularjobs = () => {
       query: 'React developer',
       num_pages: 1
     })
-   
+  const[selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id)
+  }
 
   return (
     <View style={styles.container}>
@@ -36,6 +41,8 @@ const Popularjobs = () => {
             renderItem={({ item }) => (
               <PopularJobCard
                 item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
               />
             )}
             keyExtractor={item => item?.job_id}
